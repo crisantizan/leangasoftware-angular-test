cbf94b59c7d73a7629bee2fe9cca9954d820f201
Cristian Santiz <crisantizan@gmail.com>
2020-02-26 18:09:34 -0500

    feat: add /posts route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,23 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "",
+    redirectTo: "posts",
+    pathMatch: 'full'
+  },
+  {
+    path: "posts",
+    loadChildren: () =>
+      import("./pages/posts-list/posts-list.module").then(
+        m => m.PostsListModule
+      )
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
