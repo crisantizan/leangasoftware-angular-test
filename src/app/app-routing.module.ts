7b072e0d9904a1d3083686bf5e3deebee75d6c2f
Cristian Santiz <crisantizan@gmail.com>
2020-02-27 11:32:26 -0500

    feat: go to single post from datatable

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,9 +6,11 @@ const routes: Routes = [
     path: '',
     redirectTo: 'posts',
     pathMatch: 'full',
+    data: { preload: true },
   },
   {
     path: 'posts',
+    data: { preload: true },
     loadChildren: () =>
       import('./pages/posts-list/posts-list.module').then(
         m => m.PostsListModule,
@@ -16,6 +18,7 @@ const routes: Routes = [
   },
   {
     path: 'post/:postId',
+    data: { preload: true },
     loadChildren: () =>
       import('./pages/post-detail/post-detail.module').then(
         m => m.PostDetailModule,
--- src/app/pages/posts-list/posts-list.component.ts
+++ src/app/pages/posts-list/posts-list.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { Post } from '../../models/shared.model';
 import { MatTableDataSource } from '@angular/material/table';
 import { MatPaginator } from '@angular/material/paginator';
@@ -11,7 +11,10 @@ import { MatSort } from '@angular/material/sort';
   styleUrls: ['./posts-list.component.scss'],
 })
 export class PostsListComponent implements OnInit {
-  constructor(private route: ActivatedRoute) {}
+  constructor(
+    private route: ActivatedRoute,
+    private router: Router,
+  ) {}
 
   /** columns to display in datatable component */
   public displayedColumns = ['id', 'title', 'action'];
@@ -22,16 +25,23 @@ export class PostsListComponent implements OnInit {
   @ViewChild(MatPaginator, { static: true }) paginator: MatPaginator;
 
   /* datatable sort */
-  @ViewChild(MatSort, {static: true}) sort: MatSort;
+  @ViewChild(MatSort, { static: true }) sort: MatSort;
 
   ngOnInit(): void {
     this.route.data.subscribe((data: { posts: Post[] }) => {
-      console.log('data from resolver route: ', data.posts);
       this.dataSource = new MatTableDataSource(data.posts);
       this.dataSource.paginator = this.paginator;
       this.dataSource.sort = this.sort;
     });
+
+    // this.routeListening.loading$.subscribe(loading => {
+    //   console.log('loading: ', loading);
+    // });
   }
 
   /** go to full data of post */
+  goToSinglePost(postId: number) {
+    // console.log('post id received: ', postId);
+    this.router.navigate(['/post', postId]);
+  }
 }
